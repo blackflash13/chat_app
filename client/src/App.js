49e89282d9397a4560b2b0349139f5d37dbcca7a
Vladyslav Labenskyi <80599023+blackflash13@users.noreply.github.com>
2022-09-23 13:08:39 +0300

    before OTP verify

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ function App() {
                     <Route path="/profile" element={<Profile/>}/>
                 </Routes>
             </BrowserRouter>
-        </AppContext.Provider>
+        </AppContext.Provider> 
     );
 }
 
